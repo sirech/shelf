fbb0f25ee3d69063ff9bb10a0b7d717960f6f5bd
Mario Fernandez <mario.fernandez@xing.com>
2016-09-22 18:01:24 +0200

    Update navigation after object creation

--- client/src/modules/book_navigation/reducers.js
+++ client/src/modules/book_navigation/reducers.js
@@ -1,10 +1,48 @@
+import update from 'react-addons-update'
 import { register } from 'hops'
 import { RECEIVE_YEARS } from './actions'
+import { CREATE_BOOK_SUCCESS } from '../book_creator/actions'
+
+function insertEntity (state, entityName, newEntity, key = 'id') {
+  return update(state, {
+    entities: {
+      [entityName]: {
+        $merge: { [newEntity[key]]: newEntity }
+      }
+    }
+  })
+}
+
+function insertResult (state, result, comesAfterNewElement) {
+  let index = 0
+  for (; index < state.result.length; index++) {
+    if (comesAfterNewElement(state.result[index])) {
+      break
+    }
+  }
+
+  return update(state, {
+    result: {
+      $splice: [[index, 0, result]]
+    }
+  })
+}
+
+function updateCounter (state, year) {
+  if (!state.entities.years[year]) {
+    state = insertEntity(state, 'years', { year: year, count: 0 }, 'year')
+    state = insertResult(state, year, (e) => parseInt(e) < parseInt(year))
+  }
+
+  return update(state, { entities: { years: { [year]: { count: { $apply: (n) => n + 1 } } } } })
+}
 
 function years (state = {}, action) {
   switch (action.type) {
     case RECEIVE_YEARS:
       return { ...state, ...action.years }
+    case CREATE_BOOK_SUCCESS:
+      return updateCounter(state, action.response.year)
     default:
       return state
   }
