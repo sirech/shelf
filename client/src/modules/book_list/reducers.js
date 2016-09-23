249cf1bf861ef4d9efa5c1a6a855deef11b094e8
Mario Fernandez <mario.fernandez@xing.com>
2016-09-23 14:17:17 +0200

    Show new book in the list

--- client/src/modules/book_list/reducers.js
+++ client/src/modules/book_list/reducers.js
@@ -1,12 +1,24 @@
+import update from 'react-addons-update'
 import { register } from 'hops'
 import { RECEIVE_BOOKS } from './actions'
+import { CREATE_BOOK_SUCCESS } from '../book_creator/actions'
 
 import { trackYearReducer } from '../utils/years'
+
+function addBook (books, activeYear, book) {
+  if (activeYear !== book.year) {
+    return books
+  }
+
+  return update(books, { $push: Array(book) })
+}
 
 function books (state = {}, action) {
   switch (action.type) {
   case RECEIVE_BOOKS:
     return { ...state, books: action.books }
+  case CREATE_BOOK_SUCCESS:
+    return { ...state, books: addBook(state.books, state.activeYear, action.response) }
   default:
     return state
   }
