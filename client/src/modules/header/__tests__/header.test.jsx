8f269169b290ef706420f33af9b25f3ca2556ed8
Mario Fernandez <mario.fernandez@xing.com>
2016-11-21 16:21:30 +0100

    Replace test with snapshot

--- client/src/modules/header/__tests__/header.test.jsx
+++ client/src/modules/header/__tests__/header.test.jsx
@@ -1,20 +1,13 @@
 import React from 'react'
-import { shallow } from 'enzyme'
-import Header from '../header'
+import renderer from 'react-test-renderer'
 
-const header = () => {
-  return shallow(<Header />)
-}
+import Header from '../header'
 
 describe('components', () => {
   describe('Header', () => {
-    it('renders', () => {
-      expect(header().length).toEqual(1)
-    })
-
-    it('links correctly', () => {
-      const links = header().find('Link').map(node => node.props().to)
-      expect(links).toEqual(['/', '/books', '/games'])
+    it('renders correctly', () => {
+      const header = renderer.create(<Header />)
+      expect(header.toJSON()).toMatchSnapshot()
     })
   })
 })
