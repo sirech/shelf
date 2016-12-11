147b24faa20e5f6c5f6550ffc4f95ddf9b5bc8ce
Mario Fernandez <mario.fernandez@xing.com>
2016-12-11 20:26:51 +0100

    FIX: navigation links correctly

--- client/src/modules/navigation/__tests__/item.test.jsx
+++ client/src/modules/navigation/__tests__/item.test.jsx
@@ -1,26 +1,38 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
 
 import Item from '../item'
 
-const item = (activeYear = 2015) => {
-  const props = {
-    year: 2016,
-    count: 2,
-    activeYear: activeYear
-  }
+const itemRenderer = (renderFunction) => {
+  return (entity, activeYear = 2015) => {
+    const props = {
+      year: 2016,
+      count: 2,
+      activeYear: activeYear,
+      entity: entity
+    }
 
-  return renderer.create(<Item {...props} />)
+    return renderFunction(props)
+  }
 }
+
+const item = itemRenderer((props) => renderer.create(<Item {...props} />))
 
 describe('components', () => {
   describe('Item', () => {
     it('renders', () => {
-      expect(item().toJSON()).toMatchSnapshot()
+      expect(item('books').toJSON()).toMatchSnapshot()
     })
 
     it('renders for the active item', () => {
-      expect(item(2016).toJSON()).toMatchSnapshot()
+      expect(item('books', 2016).toJSON()).toMatchSnapshot()
+    })
+
+    it('takes the entity into account for the link', () => {
+      const element = itemRenderer((props) => shallow(<Item {...props} />))
+      expect(element('games').find('Link').props().to).toEqual('/games/2016')
+      expect(element('books').find('Link').props().to).toEqual('/books/2016')
     })
   })
 })
--- client/src/modules/navigation/item.jsx
+++ client/src/modules/navigation/item.jsx
@@ -2,17 +2,18 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 import classNames from 'classnames'
 
-const Item = ({year, count, activeYear}) => (
-  <Link key={year} to={`/books/${year}`} className={classNames('list-group-item', 'list-group-item-action', { 'active': activeYear === year })}>
+const Item = ({year, count, activeYear, entity}) => (
+  <Link key={year} to={`/${entity}/${year}`} className={classNames('list-group-item', 'list-group-item-action', { 'active': activeYear === year })}>
     <span>{year}</span>
     <span className='tag tag-warning tag-pill pull-xs-right'>{count}</span>
   </Link>
 )
 
 Item.propTypes = {
-  year: PropTypes.number.isRequired,
+  activeYear: PropTypes.number.isRequired,
   count: PropTypes.number.isRequired,
-  activeYear: PropTypes.number.isRequired
+  entity: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired
 }
 
 export default Item
